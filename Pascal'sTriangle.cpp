b12681d56b65f6db23bf032122829638ed1f04e0
erdeicodrut <codrut@hackermail.com>
2016-12-18 03:13:34 +0200

    Update Pascal'sTriangle.cpp

--- Pascal'sTriangle.cpp
+++ Pascal'sTriangle.cpp
@@ -11,12 +11,12 @@ int main() {
 
 	for (int i = 0; i < r; i++) { 				//going trough each line of the matrix 
 		for (j = 0; j < i + 1; j++) {			
-			if (j == 0 || j == i) {				//setting the margins
+			if (j == 0 || j == i) {			//setting the margins
 				next[j] = 1;
 			} else {
 				next[j] = old[j-1] + old[j];	//calculating the currents
 			}
-			cout << next[j];					//showing off		
+			cout << next[j];			//showing off		
 
 		}
 		cout <<endl;
@@ -24,7 +24,7 @@ int main() {
 		for (p = 0; p < j - 1; p++) {			//making the new line the old one because we showed it in the console
 			old[p] = next[p];					
 		}
-		old[p] = 1;								//in Romania is called "treaba de carpaci"
+		old[p] = 1;					//in Romania is called "treaba de carpaci"
 	}
 
 	return 0;
